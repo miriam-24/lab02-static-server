5fe0897d37ebfa8cd08be59286e8fd7b65a8ea29
Miriam Cruz <miriiamcf@gmail.com>
2016-07-07 10:08:47 -0500

    lab05 server con html fijo

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //crearemos un modulo que sirve para crear y hacer cosas web con el modulo http
 var http = require("http");
+var fs = require('fs');
 //obteniendo informacion del entorno de ejecucion con respecto al ip y al puerto que debemos usar en nuestro server
 var PORT = process.env.PORT || 3000;
 var IP = process.env.IP || '127.0.0.1';
@@ -14,13 +15,20 @@ var server = http.createServer(function(req, res){
     //armar la respuesta http
     //armar un encabezado http
     res.writeHead(200, {
-        "Content-Type" : "text/plain", "Server" : "ITGAM@4.4.7"
+        "Content-Type" : "text/html", 
+        "Server" : "ITGAM@4.4.7"
     });
-    // enviamos la respuesta
-    res.write("HOLA SOY MIRIAM");
-    //cerrar la conexion
-    res.end();
+    //LECTURA DEL ARCHIVO A SERVIR
+    fs.readFile('./static/index.html',
+    'utf8',function(err, content) {
+        if(err){
+            res.end("<h1>ERROR DE LECTURA</h1>");
+        }else{
+            res.end(content);
+        }
+     });
 });
+    
 // poner a trabajar al server
 //puerto3000,localhost'127.0.0.1
 server.listen(PORT,IP, function(){
