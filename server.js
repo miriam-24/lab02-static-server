94e03c815b86f4a10463100707c0ba92b0131516
Miriam Cruz <miriiamcf@gmail.com>
2016-07-07 08:51:02 -0500

    Lab04 IP-PORT Server con ip y puerto tomados del entorno

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 //crearemos un modulo que sirve para crear y hacer cosas web con el modulo http
 var http = require("http");
+//obteniendo informacion del entorno de ejecucion con respecto al ip y al puerto que debemos usar en nuestro server
+var PORT = process.env.PORT || 3000;
+var IP = process.env.IP || '127.0.0.1';
+if(IP =='127.0.0.1'){
+    console.log(">----EJECUTANTO EN MODO LOCAL----<")
+}
 //crear un servidor(aquel que escucha en un puerto, escucha peticiones) basico
 //el nucleo de node nos permite crear esto
 //una funcion es una accion--collback funcion que es ejecutada---()parametros y cuerpo{}
@@ -11,12 +17,13 @@ var server = http.createServer(function(req, res){
         "Content-Type" : "text/plain", "Server" : "ITGAM@4.4.7"
     });
     // enviamos la respuesta
-    res.write("hola mundo");
+    res.write("HOLA SOY MIRIAM");
     //cerrar la conexion
     res.end();
 });
 // poner a trabajar al server
 //puerto3000,localhost'127.0.0.1
-server.listen(3000,'127.0.0.1', function(){
-    console.log(">Server listening @http://localhost:3000 ...");
+server.listen(PORT,IP, function(){
+    //HAREMOS INTERPOLACION DE CADENAS
+    console.log(`> Server listening @http://${IP}:${PORT} ...`);
 });
