6fb056b31e24694dd2e0420dc63f7f680a73b5dd
Miriam Cruz <miriiamcf@gmail.com>
2016-07-25 09:17:12 -0500

    Se conecta a base de datos pero no aparece el msj

--- internals/fortune.js
+++ internals/fortune.js
@@ -1,14 +1,42 @@
-var fortunePaper = {
-    "mensaje" :
-    "La honestidad es un regalo caro, no lo esperes de gente barata"
-};
+//1. cargando el driver de mongodb que me 
+//permitira conectarme a la base de datos
+var mongo = require("mongodb");
+//CARGO AL CLIENTE DE DRIVER
+var mongoClient = mongodb.MongoClient;
 module.exports = {
     "getFortune" : function(cb){
-        //convirtiendo el fortunepaper de objeto a su version en string
-        fortunePaper = JSON.strinify(fortunePaper);
-        //Ejecuto el callback (cb) pasandole
-        //como parametro el fortunepapaer string
-        cb(fortunePaper)
-
-    }
+        //conectando el cliente a la base de datos fortune
+        mongoClient.connect("mongodb://127.0.0.1:2701/fortune");
+        function(err, db){
+            if(err){
+                console.log("Error al conectarse a" +
+                "La base de datos:"+ "mongodb://127.0.0.1:2701/fortune");
+var fortunePaper = {
+     "mensaje" :
+        "La honestidad es un regalo caro, no lo esperes de gente barata"
+ };
+            }
+        }
+        // //convirtiendo el fortunepaper de objeto a su version en string
+        var fortunePaperResponse = JSON.stringify(fortunePaper);
+        // //Ejecuto el callback (cb) pasandole
+        // //como parametro el fortunepapaer string
+         cb(fortunePaperResponse)
+        }else{
+            //obtengo ls colleccion con la que quiero trabajar
+            var papersCollection = db collection("papers");
+            //consultos los documentos de mi collecion
+            var objetoResultado=
+            papersCollection.find({});
+            //parseo el objeto resultado en un arreglo
+            objetoResultado.toArray(function(err, papers){
+                //investigar codigo un arreglo por su tamaño para siempre tomar un elemento al azar
+                var fortunePaperResponse= JSON.stringify(papers[0]);
+                //cerrar la conexion entre el cliente
+                //y la base de datos
+                db.close()
+                //invoco al cb pasandole como parametro la respuesta
+                cb(fortunePaperResponse);
+            })
+        }
 };
